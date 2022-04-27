37ece01ec126c2d3a9d5651717b06779b94043a4
xueyinshan <suhaoshen1997@163.com>
2022-04-27 21:19:56 +0800

    add multi compute function

--- src/main/java/ComputeFactory.java
+++ src/main/java/ComputeFactory.java
@@ -1,8 +1,5 @@
 public class ComputeFactory {
 
-    private static final Add add = new Add();
-    private static final Sub sub = new Sub();
-
     public static Computable getCompute(String symbol) throws ClassNotFoundException, IllegalAccessException, InstantiationException{
         return (Computable) Class.forName(symbol).newInstance();
     }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,13 +11,19 @@ public class CalculatorTest {
         Assert.assertEquals(expectResult, actualResult);
     }
 
-
-
     @Test
     public void shouldReturn0When1Sub1() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         Calculator calculator = new Calculator();
         int actualResult = calculator.compute(1, 1, "Sub");
         int expectResult = 0;
+        Assert.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    public void shouldReturn4When2Multi2() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(2, 2, "Multi");
+        int expectResult = 4;
         Assert.assertEquals(expectResult, actualResult);
     }
 }
